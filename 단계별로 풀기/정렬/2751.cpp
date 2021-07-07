b2395cc04306cab4dd319a002847428c38ecc6b4
chajuseok <86276394+chajuseok@users.noreply.github.com>
2021-07-08 02:21:19 +0900

    Create 2751.cpp

---
+++ 단계별로 풀기/정렬/2751.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+
+	vector<int> a;
+
+	int n , b;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> b;
+		a.push_back(b);
+	}
+	sort(a.begin(), a.end());
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << "\n";
+	}
+
+}
