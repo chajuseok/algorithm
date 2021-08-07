11c8abc14474b2f3ad9e1b71be6d14d3fbb1077c
chajuseok <https://github.com/chajuseok/baekjoon.git>
2021-08-08 00:38:42 +0900

    Update 2108.cpp

--- 단계별로 풀기/정렬/2108.cpp
+++ 단계별로 풀기/정렬/2108.cpp
@@ -1,53 +1,26 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include<map>
+#include<queue>
 #include<cmath>
 
 using namespace std;
 
 int main() {
 
-	vector<int> a;
-	int n, t;
-	cin >> n;
-	map<int, int> m;
-	map<int, int> ::iterator it;
-	vector<int> temp;
-	int sum = 0;
-	int max = 0;
-	for (int i = 0; i < n; i++) {
-		cin >> t;
-		a.push_back(t);
-		m[t]++;
-		sum += t;
-	}
-	sort(a.begin(), a.end());
 
-	for (it = m.begin(); it != m.end(); it++) {
-		if (it->second > max) {
-			max = it->second;
-		}
+	int n;
+	cin >> n;
+	priority_queue<int, vector<int>, less<int>> q;
+
+	while (n > 0) {
+		q.push(n % 10);
+		n = n / 10;
 	}
-	for (it = m.begin(); it != m.end(); it++) {
-		if (it->second == max) {
-			temp.push_back(it->first);
-		}
+	while (!q.empty()) {
+		cout << q.top();
+		q.pop();
 	}
-	
-	
-	
-	cout << round((double)sum / n )<< endl;
-	cout << a[(n / 2)] << endl;
-	if (temp.size() == 1) {
-		cout << temp[0] << endl;
-	}
-	else {
-		cout << temp[1] << endl;
-	}
-	cout << a[n - 1] - a[0];
-	
-	
 	
 
 }
