e2bf4ac39040a8426eccae9a1440eeea9fe0d1c0
chajuseok <https://github.com/chajuseok/baekjoon.git>
2021-08-18 01:52:40 +0900

    Create 9251.cpp

---
+++ 단계별로 풀기/동적 계획법1/9251.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<math.h>
+
+using namespace std;
+
+int dp[1001][1001];
+
+
+int main() {
+	string a;
+	string b;
+	int answer = 0;
+	cin >> a >> b;
+	for (int i = 1; i <= a.size(); i++) {
+		for (int j = 1; j <= b.size(); j++) {
+			if (a[i-1] == b[j-1]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+		}
+	}
+	cout << dp[a.size()][b.size()];
+}
